f415ac8eb4b95d688d173857f485ba012c3d42d4
e543 <georgy_lisunov@mail.ru>
2021-04-23 13:20:08 +0700

    барьеры заменены на мьютексы

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 const size_t N = 500;				// count of records
 const size_t threads_count = 2;
 int shared_mem = 0;					// data
-boost::barrier bar1(threads_count);
-boost::barrier bar2(threads_count);
 
+boost::mutex mtx_write;
+boost::mutex mtx_read;
 
 void write() {
 	// Enter
@@ -15,24 +15,24 @@ void write() {
 		{
 			boost::this_thread::sleep(boost::posix_time::milliseconds(10));	// time to calculate new data
 
-			bar1.wait();  // block to write data
+			mtx_write.lock(); // block to write data
 			{
 				std::cout << "Calculated data:\t" << ++shared_mem << "\t";		
 			}
-			bar2.wait();
+			mtx_read.unlock();
 		}
 	}
-	bar1.wait();	
+	mtx_write.unlock();
 }
 void read() {
-	bar1.wait(); 
+	mtx_read.unlock();
 	for (std::size_t i = 0; i < N; ++i) {
 		{
-			bar2.wait();  // block to read data
+			mtx_read.lock();  // block to read data
 			{
 				std::cout << "Write to file:\t" << shared_mem << std::endl;					
 			}
-			bar1.wait();
+			mtx_write.unlock();
 		}
 	}
 	// Exit
