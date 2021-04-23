62ed36bdc598711afc86848969d63a400a48d8c7
e543 <georgy_lisunov@mail.ru>
2021-04-23 13:27:28 +0700

    some changes

--- main.cpp
+++ main.cpp
@@ -3,11 +3,10 @@
 #include <boost/thread.hpp>
 
 const size_t N = 500;				// count of records
-const size_t threads_count = 2;
 int shared_mem = 0;					// data
 
-boost::mutex mtx_write;
 boost::mutex mtx_read;
+boost::mutex mtx_write;
 
 void write() {
 	// Enter
@@ -15,24 +14,24 @@ void write() {
 		{
 			boost::this_thread::sleep(boost::posix_time::milliseconds(10));	// time to calculate new data
 
-			mtx_write.lock(); // block to write data
+			mtx_read.lock();  // block to write data
 			{
 				std::cout << "Calculated data:\t" << ++shared_mem << "\t";		
 			}
-			mtx_read.unlock();
+			mtx_write.unlock();
 		}
 	}
-	mtx_write.unlock();
+	mtx_read.unlock();
 }
 void read() {
-	mtx_read.unlock();
+	mtx_write.unlock();
 	for (std::size_t i = 0; i < N; ++i) {
 		{
-			mtx_read.lock();  // block to read data
+			mtx_write.lock();  // block to read data
 			{
 				std::cout << "Write to file:\t" << shared_mem << std::endl;					
 			}
-			mtx_write.unlock();
+			mtx_read.unlock(); // block to read data
 		}
 	}
 	// Exit
